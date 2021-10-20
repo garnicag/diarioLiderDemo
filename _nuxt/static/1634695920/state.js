11c5a862197aad7e635d564be1256ef29e6120dd
Gabriel Garnica <base@gbr.pw>
2021-10-19 21:12:05 -0500

    894892b

---
+++ _nuxt/static/1634695920/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FdiarioLiderDemo\u002F_nuxt\u002Fstatic\u002F1634695920",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002FdiarioLiderDemo\u002F",assetsPath:"\u002FdiarioLiderDemo\u002F_nuxt\u002F",cdnURL:a}}}}(null));
